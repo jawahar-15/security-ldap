fa24814985efc99bfc901bca734ef7ee7d2799b8
Jawahar Sridharan <jawahars1506@gmail.com>
2021-10-03 11:28:32 +0530

    Ldap Security

---
+++ src/main/java/spring/security/ldapsecurity/HomeController.java
@@ -0,0 +1,24 @@
+package spring.security.ldapsecurity;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@GetMapping("/home")
+	public String home() {
+		return "<h1> Welcome Home ! </h1>";
+	}
+	
+	@GetMapping("/user")
+	public String user() {
+		return "<h1> Welcome User ! </h1>";
+	}
+
+	@GetMapping("/admin")
+	public String admin() {
+		return "<h1> Welcome Admin ! </h1>";
+	}
+
+}
